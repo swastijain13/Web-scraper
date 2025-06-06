68deeb379bd9b1c3494c620cd1f0827f407ba77c
Swasti Jain <swasti.j@consultadd.com>
2025-06-06 20:23:37 +0530

    Prepare for deployment on Render

--- webScraper/settings.py
+++ webScraper/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-&lcf5=*31wl@hi=63#e5p95yp%ybyo@kv_o@786o+*7^eebohq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-scraper-1lip.onrender.com"]
 
 
 # Application definition
@@ -116,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
